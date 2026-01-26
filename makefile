d89c4e6c6aa5328dbd2b692b146ea7637e62f01f
Christopher Pina <49688431+Christopherpina2022@users.noreply.github.com>
2026-01-25 18:19:08 -0600

    redoing rawInput

--- makefile
+++ makefile
@@ -1,6 +1,6 @@
 debug: src/main.c
 	gcc -g src/main.c src/XInput.c src/input.c -lxinput -Iinclude -o cdebug
 rawdebug: src/rawInput.c
-	gcc -g src/rawInput.c -lxinput -Iinclude -o cdebug -mconsole
+	gcc -g src/rawInput.c -Iinclude -o cdebug -mconsole -lhid
 release: src/main.c
 	gcc src/main.c src/XInput.c src/input.c -lxinput -Iinclude -O3 cdebug
--- src/rawInput.c
+++ src/rawInput.c
@@ -1,123 +1,71 @@
 #include <windows.h>
 #include <stdio.h>
-#include <stdint.h>
-#include <string.h>
 
-// HWND = Handle to a window
-// Also temporarily here for testing
 static HWND g_hwnd;
-static int g_running = 1;
 
-//static GamepadState controllers[MAX_CONTROLLERS];
-
-static LRESULT CALLBACK RawInputWndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
-    printf("WndProc msg: 0x%X\n", msg);
-    fflush(stdout);
-    switch (msg)
-    {
-        case WM_INPUT:
-        {
-            printf("WM_INPUT received\n");
-            UINT size = 0;
-            GetRawInputData((HRAWINPUT)lParam, RID_INPUT,NULL, &size, sizeof(RAWINPUTHEADER));
-            BYTE buffer[256];
-
-            if (size > sizeof(buffer)) {
-                printf("ERROR: size is bigger than buffer");
-                break;
-            }
-            if (GetRawInputData((HRAWINPUT)lParam, RID_INPUT, buffer, &size, sizeof(RAWINPUTHEADER)) != size) {
-                printf("ERROR: Raw Input data is not the same size as buffer");
-                break;
-            }
-
-            // Initialize rawInput to our buffer
-            RAWINPUT* raw = (RAWINPUT*)buffer;
-
-            if (raw->header.dwType == RIM_TYPEHID)
-            {
-                RAWHID* hid = &raw->data.hid;
-
-                // I just now want the controller report
-                printf("HID report: %u bytes\n", hid->dwSizeHid);
-                fflush(stdout);
-
-                // TODO: Parse controller Info
-            }
-
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+    switch (msg) {
+        case WM_INPUT: {
+            printf("WM_INPUT received!\n");
             break;
         }
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            break;
         default:
             return DefWindowProc(hwnd, msg, wParam, lParam);
     }
-
     return 0;
 }
 
-int raw_init() {
-    // Collect data needed for Window
+int rawInit() {
+    // Create a hidden window to receive WM_INPUT
     HINSTANCE hInstance = GetModuleHandle(NULL);
     WNDCLASS wc = {0};
-    wc.lpfnWndProc = RawInputWndProc;
-    wc.hInstance = GetModuleHandle(NULL);
-    wc.lpszClassName = "RawInputHiddenWindow";
+    wc.lpfnWndProc = WndProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = "RawInputWindow";
 
     if (!RegisterClass(&wc)) {
-        printf("RegisterClass Failed: %lu\n", GetLastError());
-        return 0;
+        printf("RegisterClass failed: %lu\n", GetLastError());
+        return 1;
     }
-    printf("RegisterClass OK\n");
 
-    // Create our window with no parameters so Win32 can send the HID messages to us in background
-    g_hwnd = CreateWindowEx(
-        0, 
-        wc.lpszClassName, 
-        "", 
-        0, 
-        0, 0, 0, 0, 
-        NULL, 
-        NULL, 
-        hInstance, 
-        NULL
-    );
-
+    g_hwnd = CreateWindowEx(0, wc.lpszClassName, "", 0, 0, 0, 0, 0, NULL, NULL, hInstance, NULL);
     if (!g_hwnd) {
         printf("CreateWindowEx failed: %lu\n", GetLastError());
-        return 0;
+        return 1;
     }
-    printf("Hidden window created: %p\n", g_hwnd);
 
-    // Register raw input device as a generic gamepad
-     // For now only look for one controller
-    RAWINPUTDEVICE rid;
-    rid.usUsagePage = 0x01;          // HID_USAGE_PAGE_GENERIC
-    rid.usUsage = 0x05;              // HID_USAGE_GENERIC_GAMEPAD
-    rid.dwFlags = RIDEV_INPUTSINK;   // holds controller input even when unfocused to window
-    rid.hwndTarget = g_hwnd;
+    RAWINPUTDEVICE Rid[2];
 
-    if (!RegisterRawInputDevices(&rid, 1, sizeof(rid))) {
-        printf("RegisterRawInputDevices failed: %lu\n", GetLastError());
-        return 0;
+    Rid[0].usUsagePage = 0x01;
+    Rid[0].usUsage = 0x05; // Gamepad
+    Rid[0].dwFlags = RIDEV_INPUTSINK; // receive input even if unfocused
+    Rid[0].hwndTarget = g_hwnd;
+
+    Rid[1].usUsagePage = 0x01;
+    Rid[1].usUsage = 0x04; // Joystick
+    Rid[1].dwFlags = RIDEV_INPUTSINK;
+    Rid[1].hwndTarget = g_hwnd;
+
+    if (RegisterRawInputDevices(Rid, 2, sizeof(RAWINPUTDEVICE)) == FALSE) {
+        printf("Registration Failed: %lu\n", GetLastError());
+        return 1;
     }
-    printf("RegisterRawInputDevices OK\n");
 
-    return 1;
+    printf("Registration Completed.\n");
+    return 0;
 }
 
 int main() {
-    if (!raw_init()) {
-        printf("Raw Init Failed\n");
-        return 1;
-    }
-    printf("RawInput initialized. Waiting for input...\n");
+    if (rawInit() != 0) return 1;
 
-    while (g_running) {
-        MSG msg;
-        while (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
-        }
-        Sleep(1);
+    MSG msg;
+    while (GetMessage(&msg, NULL, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
     }
+
     return 0;
 }
