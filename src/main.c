11770fd88ceaa248eabdfed706d09514713ddcf0
Christopher Pina <49688431+Christopherpina2022@users.noreply.github.com>
2026-02-07 19:43:35 -0600

    Added support for multiple controllers

--- src/main.c
+++ src/main.c
@@ -3,10 +3,13 @@
 #include <stdint.h>
 #include <input.h>
 
-typedef enum {
-    INPUT_BACKEND_XINPUT,
-    INPUT_BACKEND_RAWINPUT
-} InputBackend;
+/* This defines the space we allocate for the controller, also really useful
+for offseting the spacing between controllers in RenderController. padding can also allow
+us to add a cosmetic flair such as filling the border with a character or something. */ 
+#define CONTROLLER_PANEL_WIDTH 30
+#define CONTROLLER_PANEL_PADDING 2
+
+#define CONTROLLER_STRIDE (CONTROLLER_PANEL_WIDTH + CONTROLLER_PANEL_PADDING)
 
 typedef struct {
     int height;
@@ -25,58 +28,60 @@ void toBuffer (ConsoleScreen *screen, int x, int y, const char *string) {
     }
 }
 
-void renderController(ConsoleScreen *screen, const GamepadState *state) {
+void renderController(ConsoleScreen *screen, const GamepadState *state, int conIndex) {
     char tempBuffer[32];
+    int xOffset = conIndex * CONTROLLER_STRIDE;
 
     if (!state || !state->connected) {
-        toBuffer(screen, 0, 0, "Controller is not connected.");
+        toBuffer(screen, xOffset, 0, "No Controller.");
         return;
     }
 
-    toBuffer(screen, 0, 0, "Controller");
-    toBuffer(screen, 0, 1, "============");
+    sprintf(tempBuffer, "Controller %d", conIndex);
+    toBuffer(screen, xOffset, 0, tempBuffer);
+    toBuffer(screen, xOffset, 1, "============");
 
     sprintf(tempBuffer, "D-UP: %-8s", (state->buttons & BTN_DPAD_UP) ? "Pressed" : "Released");
-    toBuffer(screen, 0, 3, tempBuffer);
+    toBuffer(screen, xOffset, 3, tempBuffer);
 
     sprintf(tempBuffer, "D-DOWN: %-8s", (state->buttons & BTN_DPAD_DOWN) ? "Pressed" : "Released");
-    toBuffer(screen, 0, 4, tempBuffer);
+    toBuffer(screen, xOffset, 4, tempBuffer);
 
     sprintf(tempBuffer, "D-LEFT: %-8s", (state->buttons & BTN_DPAD_LEFT) ? "Pressed" : "Released");
-    toBuffer(screen, 0, 5, tempBuffer);
+    toBuffer(screen, xOffset, 5, tempBuffer);
 
     sprintf(tempBuffer, "D-RIGHT: %-8s", (state->buttons & BTN_DPAD_RIGHT) ? "Pressed" : "Released");
-    toBuffer(screen, 0, 6, tempBuffer);
+    toBuffer(screen, xOffset, 6, tempBuffer);
 
     sprintf(tempBuffer, "LX: %+0.3f", state->axes[INPUT_AXIS_LEFT_X]);
-    toBuffer(screen, 0, 8, tempBuffer);
+    toBuffer(screen, xOffset, 8, tempBuffer);
 
     sprintf(tempBuffer, "LY: %+0.3f", state->axes[INPUT_AXIS_LEFT_Y]);
-    toBuffer(screen, 0, 9, tempBuffer);
+    toBuffer(screen, xOffset, 9, tempBuffer);
 
     sprintf(tempBuffer, "RX: %+0.3f", state->axes[INPUT_AXIS_RIGHT_X]);
-    toBuffer(screen, 0, 11, tempBuffer);
+    toBuffer(screen, xOffset, 11, tempBuffer);
 
     sprintf(tempBuffer, "RY: %+0.3f", state->axes[INPUT_AXIS_RIGHT_Y]);
-    toBuffer(screen, 0, 12, tempBuffer);
+    toBuffer(screen, xOffset, 12, tempBuffer);
 
     sprintf(tempBuffer, "LT/Z: %+0.3f", state->axes[INPUT_AXIS_LT]);
-    toBuffer(screen, 0, 14, tempBuffer);
+    toBuffer(screen, xOffset, 14, tempBuffer);
 
     sprintf(tempBuffer, "RT/RZ: %+0.3f", state->axes[INPUT_AXIS_RT]);
-    toBuffer(screen, 0, 15, tempBuffer);
+    toBuffer(screen, xOffset, 15, tempBuffer);
 
     sprintf(tempBuffer, "A: %-8s", (state->buttons & BTN_A) ? "Pressed" : "Released");
-    toBuffer(screen, 0, 17, tempBuffer);
+    toBuffer(screen, xOffset, 17, tempBuffer);
 
     sprintf(tempBuffer, "B: %-8s", (state->buttons & BTN_B) ? "Pressed" : "Released");
-    toBuffer(screen, 0, 18, tempBuffer);
+    toBuffer(screen, xOffset, 18, tempBuffer);
 
     sprintf(tempBuffer, "X: %-8s", (state->buttons & BTN_X) ? "Pressed" : "Released");
-    toBuffer(screen, 0, 19, tempBuffer);
+    toBuffer(screen, xOffset, 19, tempBuffer);
 
     sprintf(tempBuffer, "Y: %-8s", (state->buttons & BTN_Y) ? "Pressed" : "Released");
-    toBuffer(screen, 0, 20, tempBuffer);
+    toBuffer(screen, xOffset, 20, tempBuffer);
 }
 
 void flushBuffer(ConsoleScreen *screen) {
@@ -122,11 +127,17 @@ int main () {
     
     while (1) {
         clearRegion(&screen);
-
-        // We are only grabbing data on the first controller we see for now
         input_update();
-        const GamepadState *padState = input_get_gamepad(0);
-        renderController(&screen, padState);
+
+        for (int conIndex = 0; conIndex < MAX_CONTROLLERS; conIndex++) {
+            int xOffset = conIndex * CONTROLLER_STRIDE;
+
+            if (xOffset + CONTROLLER_PANEL_WIDTH >= screen.width)
+                break;
+
+            const GamepadState *pad = input_get_gamepad(conIndex);
+            renderController(&screen, pad, conIndex);
+        }
         flushBuffer(&screen);
         Sleep(16);
 
